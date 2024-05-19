1174ec3bc3adcc07b851218b19cc1382eb3b5a60
SitoCH <sito@grignola.ch>
2024-05-19 23:18:53 +0200

    Cleanup

--- hyperliquid_bot.py
+++ hyperliquid_bot.py
@@ -26,15 +26,9 @@ class HyperliquidBot():
 
         self.telegram_app = Application.builder().token(telegram_token).build()
 
-        # application.add_handler(CommandHandler("start", self.start))
-        # application.add_handler(CommandHandler("orders", self.get_orders))
         self.telegram_app.add_handler(CommandHandler("balance", self.get_balance))
 
         self.telegram_app.run_polling(allowed_updates=Update.ALL_TYPES)
-
-    # def split_message(self, message, max_length=4096):
-    #     """Splits a message into chunks of max_length."""
-    #     return [message[i:i + max_length] for i in range(0, len(message), max_length)]
 
     def on_user_events(self, user_events: UserEventsMsg) -> None:
         user_events_data = user_events["data"]
@@ -50,37 +44,6 @@ class HyperliquidBot():
             message = f"Failed to fetch balance: {str(e)}"
 
         await update.message.reply_text(message)
-
-    # async def get_orders(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    #     if len(context.args) != 1:
-    #         await update.message.reply_text('Usage: /orders <your_eth_address>')
-    #         return
-
-    #     user_address = context.args[0]
-    #     payload = {
-    #         "type": "openOrders",
-    #         "user": user_address
-    #     }
-    #     headers = {
-    #         "Content-Type": "application/json"
-    #     }
-
-    #     try:
-    #         response = requests.post(api_url, json=payload, headers=headers)
-    #         if response.status_code == 200:
-    #             orders = response.json()
-    #             if orders:
-    #                 message = "Your open orders:\n\n" + "\n".join([str(order) for order in orders])
-    #             else:
-    #                 message = "You have no open orders."
-    #         else:
-    #             message = f"Failed to fetch orders: {response.status_code}"
-    #     except Exception as e:
-    #         message = f"An error occurred: {str(e)}"
-
-    #     for chunk in self.split_message(message):
-    #         await update.message.reply_text(chunk)
-
 
 if __name__ == '__main__':
     bot = HyperliquidBot()
