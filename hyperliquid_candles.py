0652b3ca5d13b57b426c0bbc954159c5060837b2
SitoCH <sito@grignola.ch>
2024-07-27 00:07:41 +0200

    Include TA on 4h candles

--- hyperliquid_candles.py
+++ hyperliquid_candles.py
@@ -2,11 +2,17 @@ import os
 import time
 import pandas as pd
 import pandas_ta as ta
+
 from tabulate import simple_separated_format, tabulate
+
+from logging_utils import logger
+
 from telegram.ext import ContextTypes
 from telegram_utils import telegram_utils
 from telegram.constants import ParseMode
+
 from hyperliquid_utils import hyperliquid_utils
+
 from utils import fmt
 
 
@@ -18,17 +24,21 @@ async def analyze_candles(context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def analyze_candles_for_coin(context, coin: str) -> None:
+    logger.info(f"Running TA for {coin}")
     try:
         now = int(time.time() * 1000)
-        five_days_ago = now - 7 * 86400000
 
-        candles = hyperliquid_utils.info.candles_snapshot(coin, "1h", five_days_ago, now)
-        df = prepare_dataframe(candles)
+        candles_1h = hyperliquid_utils.info.candles_snapshot(coin, "1h", now - 7 * 86400000, now)
+        candles_4h = hyperliquid_utils.info.candles_snapshot(coin, "4h", now - 14 * 86400000, now)
+        
+        df_1h = prepare_dataframe(candles_1h)
+        aroon_flip_1h, supertrend_flip_1h = apply_indicators(df_1h)
 
-        aroon_flip_detected, supertrend_flip_detected = apply_indicators(df)
+        df_4h = prepare_dataframe(candles_4h)
+        aroon_flip_4h, supertrend_flip_4h = apply_indicators(df_4h)
 
-        if aroon_flip_detected or supertrend_flip_detected:
-            await send_trend_change_message(context, df, coin)
+        if aroon_flip_1h or supertrend_flip_1h or aroon_flip_4h or supertrend_flip_4h:
+            await send_trend_change_message(context, df_1h, df_4h, coin)
     except Exception as e:
         await context.bot.send_message(
             text=f"Failed to analyze candles for {coin}: {str(e)}",
@@ -65,28 +75,26 @@ def apply_indicators(df: pd.DataFrame) -> tuple:
     return df['Aroon_Flip_Detected'].iloc[-1], df['SuperTrend_Flip_Detected'].iloc[-1]
 
 
-async def send_trend_change_message(context, df: pd.DataFrame, coin: str) -> None:
-    aroon_up_prev = df['Aroon_Up'].iloc[-2]
-    aroon_down_prev = df['Aroon_Down'].iloc[-2]
-    aroon_up = df['Aroon_Up'].iloc[-1]
-    aroon_down = df['Aroon_Down'].iloc[-1]
-    aroon_trend_prev = "uptrend" if aroon_up_prev > aroon_down_prev else "downtrend"
-    aroon_trend = "uptrend" if aroon_up > aroon_down else "downtrend"
-
-    supertrend_prev = df['SuperTrend'].iloc[-2]
-    supertrend = df['SuperTrend'].iloc[-1]
-    supertrend_trend_prev = "uptrend" if df['InUptrend'].iloc[-2] else "downtrend"
-    supertrend_trend = "uptrend" if df['InUptrend'].iloc[-1] else "downtrend"
+async def send_trend_change_message(context, df_1h: pd.DataFrame, df_4h: pd.DataFrame, coin: str) -> None:
+    results_1h = get_ta_results(df_1h)
+    results_4h = get_ta_results(df_4h)
 
     table = tabulate(
         [
-            ["Aroon: ", "", ""],
-            ["Trend ", aroon_trend_prev, aroon_trend],
-            ["Up ", fmt(aroon_up_prev), fmt(aroon_up)],
-            ["Down ", fmt(aroon_down_prev), fmt(aroon_down)],
-            ["Supertrend: ", "", ""],
-            ["Trend ", supertrend_trend_prev, supertrend_trend],
-            ["Value ", round(supertrend_prev, 2 if supertrend_prev > 1 else 4), round(supertrend_prev, 2 if supertrend > 1 else 4)],
+            ["Aroon 1h: ", "", ""],
+            ["Trend ", results_1h["aroon_trend_prev"], results_1h["aroon_trend"]],
+            ["Up ", fmt(results_1h["aroon_up_prev"]), fmt(results_1h["aroon_up"])],
+            ["Down ", fmt(results_1h["aroon_down_prev"]), fmt(results_1h["aroon_down"])],
+            ["Supertrend 1h: ", "", ""],
+            ["Trend ", results_1h["supertrend_trend_prev"], results_1h["supertrend_trend"]],
+            ["Value ", round(results_1h["supertrend_prev"], 2 if results_1h["supertrend_prev"] > 1 else 4), round(results_1h["supertrend_prev"], 2 if results_1h["supertrend"] > 1 else 4)],
+            ["Aroon 4h: ", "", ""],
+            ["Trend ", results_4h["aroon_trend_prev"], results_4h["aroon_trend"]],
+            ["Up ", fmt(results_4h["aroon_up_prev"]), fmt(results_4h["aroon_up"])],
+            ["Down ", fmt(results_4h["aroon_down_prev"]), fmt(results_4h["aroon_down"])],
+            ["Supertrend 4h: ", "", ""],
+            ["Trend ", results_4h["supertrend_trend_prev"], results_4h["supertrend_trend"]],
+            ["Value ", round(results_4h["supertrend_prev"], 2 if results_4h["supertrend_prev"] > 1 else 4), round(results_4h["supertrend_prev"], 2 if results_4h["supertrend"] > 1 else 4)],
         ],
         headers=["", "Previous", "Current"],
         tablefmt=simple_separated_format(' '),
@@ -104,3 +112,30 @@ async def send_trend_change_message(context, df: pd.DataFrame, coin: str) -> Non
         parse_mode=ParseMode.HTML,
         chat_id=telegram_utils.telegram_chat_id
     )
+
+
+def get_ta_results(df):
+    aroon_up_prev = df['Aroon_Up'].iloc[-2]
+    aroon_down_prev = df['Aroon_Down'].iloc[-2]
+    aroon_up = df['Aroon_Up'].iloc[-1]
+    aroon_down = df['Aroon_Down'].iloc[-1]
+    aroon_trend_prev = "uptrend" if aroon_up_prev > aroon_down_prev else "downtrend"
+    aroon_trend = "uptrend" if aroon_up > aroon_down else "downtrend"
+
+    supertrend_prev = df['SuperTrend'].iloc[-2]
+    supertrend = df['SuperTrend'].iloc[-1]
+    supertrend_trend_prev = "uptrend" if df['InUptrend'].iloc[-2] else "downtrend"
+    supertrend_trend = "uptrend" if df['InUptrend'].iloc[-1] else "downtrend"
+
+    return {
+        "aroon_up_prev": aroon_up_prev,
+        "aroon_down_prev": aroon_down_prev,
+        "aroon_up": aroon_up,
+        "aroon_down": aroon_down,
+        "aroon_trend_prev": aroon_trend_prev,
+        "aroon_trend": aroon_trend,
+        "supertrend_prev": supertrend_prev,
+        "supertrend": supertrend,
+        "supertrend_trend_prev": supertrend_trend_prev,
+        "supertrend_trend": supertrend_trend
+    }
