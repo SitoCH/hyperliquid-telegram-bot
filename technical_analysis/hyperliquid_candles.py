e51d4fa448246399ad7efc0e53d061841f16e114
SitoCH <sito@grignola.ch>
2025-01-13 23:22:09 +0100

    Increase job scheduling delay for candle analysis to improve processing time

--- technical_analysis/hyperliquid_candles.py
+++ technical_analysis/hyperliquid_candles.py
@@ -76,7 +76,7 @@ async def analyze_candles(context: ContextTypes.DEFAULT_TYPE) -> None:
     for coin in coins_to_analyze:
         context.application.job_queue.run_once( # type: ignore
             analyze_candles_for_coin_job,
-            when=loop * 7,
+            when=loop * 8,
             data={"coin": coin},
             job_kwargs={'misfire_grace_time': 180}
         )
