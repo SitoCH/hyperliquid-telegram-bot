7240051ade784c05e2758cdb8e4b09d6c8afba9f
SitoCH <sito@grignola.ch>
2025-03-30 23:51:44 +0200

    Enhance logging format to include a custom date format for improved readability

--- logging_utils.py
+++ logging_utils.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 httpx_logger = logging.getLogger("httpx")
